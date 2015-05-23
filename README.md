495a1ef6edfeb6691dd21a9acc3cdeda784d9c42
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2015-05-23 09:34:49 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+TDD java template
+=======================
+
+This project is template for training for TDD with php.
+
+Getting started
+---------------
+
+    mvn install
+
+For running tests
+
+    mvn test
+
+Stiring Calculator Kata - https://docs.google.com/document/d/1UXGR9mJp9aV4Khn-hyZmFu-tk4rMEcvHaJdarsg7qYE/edit
+
+Online IDE
+[Nitrous.io](https://www.nitrous.io/join/pZ5KOhiHTRU?utm_source=nitrous.io&utm_medium=copypaste&utm_campaign=referral)
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,30 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class StringCalculatorTest extends TestCase {
+
+    private StringCalculator sut;
+
+    @Test
+    public void testPasses() {
+        String expected = "Hello, JUnit!";
+        String hello = "Hello, JUnit!";
+        assertEquals(hello, expected);
+    }
+
+//    @Test
+//    public void testFails() {
+//        // The worlds most obvious bug:
+//        assertTrue(false);
+//    }
+
+    @Test
+    public void testArray() {
+        int [] array1 = new int[] {1, 2, 3};
+        int [] array2 = new int[] {1, 2, 3};
+        assertArrayEquals(array1, array2);
+    }
+
+}
